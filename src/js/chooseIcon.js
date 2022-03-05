a04a74c9d6a0e08fde9c36a50c839b5920e7b4f0
Jan Miksovsky <jan@miksovsky.com>
2022-03-05 11:54:05 -0800

    Origami icons

---
+++ src/js/chooseIcon.js
@@ -0,0 +1,18 @@
+import { ExplorableGraph } from "@explorablegraph/explorable";
+
+// Pick an icon for a page that is quasi-random but stable across visits.
+export default async function chooseIcon(page, icons) {
+  // Treating the icons as buckets, hash the page URL to a bucket (icon).
+  const keys = await ExplorableGraph.keys(icons);
+  const pageHash = hash(page, keys.length);
+  const icon = keys[pageHash];
+  return icon;
+}
+
+function hash(key, buckets) {
+  let hash = 0;
+  for (let i = 0; i < key.length; i++) {
+    hash = (hash + key.charCodeAt(i) * (i + 1)) % buckets;
+  }
+  return hash;
+}
