4f3ace57668f79708ecc19e565e912eb8adf2bd3
Jan Miksovsky <jan@miksovsky.com>
2022-03-14 11:24:58 -0700

    Promote inline to a builtin

--- src/js/inline.js
+++
@@ -1,6 +0,0 @@
-import { orit } from "@explorablegraph/explorable";
-
-export default async function inline(buffer) {
-  const result = await orit.call(this, buffer, null, true);
-  return result;
-}
