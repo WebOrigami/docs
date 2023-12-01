da7d591c56179dfaf7a172c7d7b3a056f7632954
Jan Miksovsky <jan@miksovsky.com>
2023-12-01 11:05:50 -0800

    Track changes

--- src/js/separatedList.js
+++ src/js/separatedList.js
@@ -1,11 +1,11 @@
 import { Tree } from "@graphorigami/async-tree";
-import { treeMap } from "@graphorigami/origami";
+import { map } from "@graphorigami/origami";
 
 export default async function separatedList(list, separator, valueMap) {
   if (!list) {
     return undefined;
   }
-  const mapped = treeMap.call(this, list, valueMap);
+  const mapped = map.call(this, list, valueMap);
   const values = await Tree.values(mapped);
   const trimmed = values.map((value) => value.trim());
   const result = trimmed.join(separator);
