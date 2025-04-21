df8bd9e217133bc4c9b65169fb53ae483edb2ca7
Jan Miksovsky <jan@miksovsky.com>
2025-04-21 16:11:23 -0700

    Add header links

---
+++ src/js/headerLinks.js
@@ -0,0 +1,19 @@
+import { toString } from "@weborigami/async-tree";
+/**
+ * Scans the HTML for header tags with `id` attributes and returns adjusted HTML
+ * in which each header tag has a link to itself.
+ *
+ * @param {string} html
+ */
+export default function headerLinks(html) {
+  html = toString(html);
+  const headerRegex =
+    /(?<open><h([1-6])[^>]*id="(?<id>[^"]+)"[^>]*>)(?<children>.*?)(?<close><\/h\2>)/g;
+  return html.replace(
+    headerRegex,
+    (match, open, level, id, children, close) => {
+      const link = ` <a href="#${id}">#</a>`;
+      return `${open}${children}${link}${close}`;
+    }
+  );
+}
