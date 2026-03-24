1302a86522250073cfee8052c25d795d08f46f91
Jan Miksovsky <jan@miksovsky.com>
2026-03-24 10:41:42 -0700

    Simplify icon choosing by using Node hash

--- src/js/chooseIcon.js
+++ src/js/chooseIcon.js
@@ -1,26 +1,9 @@
 import { Tree } from "@weborigami/async-tree";
+import { createHash } from "node:crypto";
 
 // Pick an icon for a page that is quasi-random but stable across visits.
 export default async function chooseIcon(filePath, icons) {
-  if (!filePath || !icons) {
-    return undefined;
-  }
-
-  // Get the keys that end with .svg
-  const keys = Array.from(await Tree.keys(icons)).filter((key) =>
-    key.endsWith(".svg")
-  );
-
-  // Treating the icons as buckets, hash the page URL to a bucket (icon).
-  const pageHash = hash(filePath, keys.length);
-  const icon = keys[pageHash];
-  return icon;
-}
-
-function hash(key, buckets) {
-  let hash = 0;
-  for (let i = 0; i < key.length; i++) {
-    hash = (hash + key.charCodeAt(i) * (i + 1)) % buckets;
-  }
-  return hash;
+  const iconNames = await Tree.keys(icons);
+  const hash = createHash("sha256").update(filePath).digest().readUInt32LE(0);
+  return iconNames[hash % iconNames.length];
 }
