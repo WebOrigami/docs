358a2fc1a4a71ab0318dc19c2576a269f89daa41
Jan Miksovsky <jan@miksovsky.com>
2026-05-25 15:42:04 -0700

    Use Origami.randomFrom

--- src/js/chooseIcon.js
+++ src/js/chooseIcon.js
@@ -1,11 +1,9 @@
 import { Tree } from "@weborigami/async-tree";
 import { Origami } from "@weborigami/origami";
-import { createHash } from "node:crypto";
 
 // Pick an icon for a page that is quasi-random but stable across visits.
 export default async function chooseIcon(filePath, icons) {
   const iconNames = await Tree.keys(icons);
-  const hash = createHash("sha256").update(filePath).digest().readUInt32LE(0);
   const random = Origami.randomFrom(filePath);
-  return iconNames[hash % iconNames.length];
+  return iconNames[random % iconNames.length];
 }
